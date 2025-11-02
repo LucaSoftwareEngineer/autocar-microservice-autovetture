943d853607b325aed35312830f1be3d39708df9b
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-11-03 00:47:18 +0100

    initial commit

---
+++ src/main/java/autocar/microservice/AutocarMicroserviceAutovettureApplication.java
@@ -0,0 +1,13 @@
+package autocar.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocarMicroserviceAutovettureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocarMicroserviceAutovettureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/autocar/microservice/AutocarMicroserviceAutovettureApplicationTests.java
@@ -0,0 +1,13 @@
+package autocar.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocarMicroserviceAutovettureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
